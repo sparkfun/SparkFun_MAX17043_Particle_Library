4d52c0f9c6e1d7ccc150e232f290316d24a31d9c
Jim Lindblom <jim@sparkfun.com>
2015-06-25 13:44:44 -0600

    Moving MAX17043 defintions to cpp. (looks cleaner in particle build.)

--- firmware/SparkFunMAX17043.cpp
+++ firmware/SparkFunMAX17043.cpp
@@ -19,7 +19,39 @@ This code is released under the MIT license.
 Distributed as-is; no warranty is given.
 ******************************************************************************/
 #include "SparkFunMAX17043.h"
-#include "MAX17043_Definitions.h"
+
+///////////////////////////////////
+// MAX17043 Register Definitions //
+///////////////////////////////////
+// All registers contain two bytes of data and span two addresses.
+#define MAX17043_VCELL    0x02 // R - 12-bit A/D measurement of battery voltage
+#define MAX17043_SOC      0x04 // R - 16-bit state of charge (SOC)
+#define MAX17043_MODE     0x06 // W - Sends special commands to IC
+#define MAX17043_VERSION  0x08 // R - Returns IC version
+#define MAX17043_CONFIG   0x0C // R/W - Battery compensation (default 0x971C)
+#define MAX17043_COMMAND  0xFE // W - Sends special comands to IC
+
+///////////////////////////////////
+// MAX17043 Config Register Bits //
+///////////////////////////////////
+#define MAX17043_CONFIG_SLEEP     7
+#define MAX17043_CONFIG_ALERT     5
+#define MAX17043_CONFIG_THRESHOLD 0
+
+/////////////////////////////////////
+// MAX17043 Mode Register Commands //
+/////////////////////////////////////
+#define MAX17043_MODE_QUICKSTART 0x4000
+
+////////////////////////////////////////
+// MAX17043 Command Register Commands //
+////////////////////////////////////////
+#define MAX17043_COMMAND_POR 0x5400
+
+////////////////////////////////
+// MAX17043 7-Bit I2C Address //
+////////////////////////////////
+#define MAX17043_ADDRESS  0x36
 
 MAX17043::MAX17043()
 {
